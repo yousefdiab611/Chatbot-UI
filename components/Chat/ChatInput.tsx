ca46e49d020d094e829ddf5dd5f0efd37b413db8
Yousef Diab <dev.daibb@gmail.com>
2024-10-18 18:31:03 +0300

    Update ChatInput.tsx

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -354,7 +354,7 @@ export const ChatInput: FC<Props> = ({
       </div>
       <div className="px-3 pt-2 pb-3 text-center text-[12px] text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6">
         <a
-          href="https://github.com/mckaywrigley/chatbot-ui"
+          href="https://github.com/yousefdiab11/chatbot-ui"
           target="_blank"
           rel="noreferrer"
           className="underline"
